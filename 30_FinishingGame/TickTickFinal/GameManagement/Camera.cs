4c853dff21590c712a78348be74b9ad772cd56b1
Thalvarian <a.vanterwisga@students.uu.nl>
2017-10-25 13:15:41 +0200

    first thingie

---
+++ 30_FinishingGame/TickTickFinal/GameManagement/Camera.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace TickTick5.GameManagement
+{
+    class Camera
+    {
+        ///* virtual camera, should also work in fullscreen mode */
+        //private static Camera virtcam;
+        //Vector2 position;
+
+    }
+}
