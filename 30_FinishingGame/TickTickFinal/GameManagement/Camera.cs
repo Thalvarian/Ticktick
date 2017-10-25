a8d43648b2f2b66957150b852fa189390ee17ab2
Thalvarian <a.vanterwisga@students.uu.nl>
2017-10-25 15:02:14 +0200

    beter werkt t nu

--- 30_FinishingGame/TickTickFinal/GameManagement/Camera.cs
+++ 30_FinishingGame/TickTickFinal/GameManagement/Camera.cs
@@ -7,11 +7,9 @@ using Microsoft.Xna.Framework;
 
 class Camera
 {
-<<<<<<< HEAD
+
     /* virtual camera, should also work in fullscreen mode */
-=======
-    ///* virtual camera, should also work in fullscreen mode */
->>>>>>> 9b012dfe9999f218762a611ac60c7ec9f15359c5
+
     private static Camera virtcam;
     Vector2 position;
 }
